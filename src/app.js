b3bc3344865330689cc3312a13fa015d6ccd8120
Bruno Lacerda <brunolcpg@gmail.com>
2020-04-09 21:49:54 -0300

    Repository CRUD Node concepts challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,56 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository)
+  return res.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs } = req.body;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if(repoIndex < 0) {
+    return res.status(400).json({ error: "Invalid repository id" });
+  }
+
+  const repository = repositories[repoIndex];
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+  repositories[repoIndex] = repository;
+  return res.json(repository);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if(repoIndex < 0) {
+    return res.status(400).json({ error: "Invalid repository id" });
+  }
+
+  repositories.splice(repoIndex, 1);
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if(repoIndex < 0) {
+    return res.status(400).json({ error: "Invalid repository id" });
+  }
+
+  repositories[repoIndex].likes += 1;
+  return res.json(repositories[repoIndex]);
 });
 
 module.exports = app;
